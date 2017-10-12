bde8ec788e58d28c42d15d269cd6236fec3261c7
Moustapha-A <moustapha.m.ahmad@gmail.com>
2017-10-12 16:47:49 +0200

    Added Axes labels in plot=TRUE

--- R/autopolate.R
+++ R/autopolate.R
@@ -110,7 +110,7 @@ autopolate = function(dataframe, timeCol, timeFrmt, valueCol, breaksGen="normal"
   }
 
   if(isTRUE(plot)){
-    plot(x,y,col=rgb(0,0.1,1,0.8))
+    plot(x,y,col=rgb(0,0.1,1,0.8) , main = "Initial Vs Interpolated", xlab = timeCol, ylab = valueCol)
     for(fnct in fncts){
       denseInterval = seq(head(fnct$argvals,1),tail(fnct$argvals,1),10)
       lines(denseInterval,fda::eval.fd(denseInterval,fnct$fd))
